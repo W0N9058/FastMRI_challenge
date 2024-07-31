6ebeddc10e2b58c108a6461d6e1d202b13fe5c3e
W0N9058 <rigel9058@snu.ac.kr>
2024-07-31 05:57:06 +0000

    bebygan upload

--- parameter_search.sh
+++ parameter_search.sh
@@ -5,11 +5,11 @@ LOG_FILE="result.log"
 # 로그 파일 초기화
 echo "" > $LOG_FILE
 
-for cascade in $(seq 1 1)
+for cascade in $(seq 1 3)
 do
-  for chans in $(seq 8 8)
+  for chans in $(seq 8 10)
   do
-    for sens_chans in $(seq 3 3)
+    for sens_chans in $(seq 3 5)
     do
       # 모델 학습
       python train.py \
